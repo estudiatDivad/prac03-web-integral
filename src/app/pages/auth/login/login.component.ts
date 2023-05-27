83c6b08ea4f1fb94c22e3fc381827dbfdcf24f85
Yop789 <2000djpz@gmail.com>
2023-05-26 18:28:49 -0600

    proyecto login

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BaseForm } from 'src/app/shared/utils/base-form';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  constructor(private fb: FormBuilder, public baseForm:BaseForm, private router:Router) {}
+
+  ngOnInit() {
+    
+  }
+
+  onLogin(){
+    this.router.navigateByUrl("/home")
+  }
+}
